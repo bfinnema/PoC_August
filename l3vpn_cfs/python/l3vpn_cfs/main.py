7e0481ecb4b399bf28ceb9a32eb442b15b512781
Stefano Novello <StefanoNovello@users.noreply.github.com>
2024-07-22 12:44:57 +0200

    Added ping test to l3vpn cfs

--- l3vpn_cfs/python/l3vpn_cfs/main.py
+++ l3vpn_cfs/python/l3vpn_cfs/main.py
@@ -1,6 +1,60 @@
 # -*- mode: python; python-indent: 4 -*-
 import ncs
 from ncs.application import Service
+from ncs.dp import Action
+from datetime import datetime
+import json
+
+class PingTest(Action):
+    @Action.action
+    def cb_action(self, uinfo, name, kp, input, output, trans):
+        self.log.info('action name: ', name)
+        results = {}
+        with ncs.maapi.single_read_trans('admin', 'python') as t:
+            service = ncs.maagic.get_node(t, kp)
+            root = ncs.maagic.get_root(t)
+            for endpoint in service.endpoint:
+                local_pe = endpoint.pe_device
+                remote_result = None
+                local_result = self.ping(root.devices.device[local_pe],
+                                                          endpoint.cpe_loopback0_address, service.vrf_name)
+                for remote in service.endpoint:
+                    if remote.pe_device != local_pe:
+                        remote_result = self.ping(root.devices.device[remote.pe_device],
+                                                          endpoint.cpe_loopback0_address, service.vrf_name)  
+                        break
+                if remote_result is None:
+                    remote_result = 'No remote PE to test from'                      
+                results[endpoint.id] = { 'local': local_result, 'remote': remote_result}
+        now = datetime.now()
+        
+        with ncs.maapi.single_write_trans('admin', 'python') as t:
+            service = ncs.maagic.get_node(t, kp)
+            service.last_ping.when = str(now)
+            del service.last_ping.endpoint
+            for endpoint in service.endpoint: 
+                last_ping_endpoint = service.last_ping.endpoint.create(endpoint.id)
+                last_ping_endpoint.local_result = results[endpoint.id]['local']
+                last_ping_endpoint.remote_result = results[endpoint.id]['remote']           
+            t.apply()
+        output.result = json.dumps(results)
+
+    def ping(self, pe, address, vrf):
+            if 'iosxr' not in pe.device_type.netconf.ned_id:
+                return "ERROR: Ping only supported on IOSXR with netconf NED"
+            self.log.info(f'PING from device: {pe.name} to address: {address} in vrf: {vrf}')
+            ping_action = pe.rpc.rpc_ping.ping
+            ping_input = ping_action.get_input()
+            dest = ping_input.destination.create()
+            dest.destination = address
+            dest.vrf_name = vrf
+            ping_output_list = ping_action(ping_input).ping_response.ipv4
+            self.log.info(repr(ping_output_list))
+            for ping_output in ping_output_list:
+                replies = ''.join((reply.result for reply in ping_output.replies.reply))
+                # only one element in list
+                return f'{pe.name} -> {address} :: {ping_output.success_rate}% success rate, replies: {replies}'
+
 
 class ServiceCallbacks(Service):
 
@@ -65,6 +119,7 @@ class Main(ncs.application.Application):
     def setup(self):
         self.log.info('Main RUNNING')
         self.register_service('l3vpn_cfs-servicepoint', ServiceCallbacks)
+        self.register_action('l3vpn_cfs-ping-test', PingTest)
 
     def teardown(self):
         self.log.info('Main FINISHED')
